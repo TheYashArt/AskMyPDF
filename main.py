f4be74cefaeb1e92f4e4b527d6969be992463e0c
TheYashArt <shelkey859@gmail.com>
2026-01-05 13:54:25 +0530

    requirement definition

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain_classic.chains import retrieval_qa
+from langchain_classic.chains import RetrievalQA
 
 with st.sidebar:
     groq_api_key = st.text_input("Groq API Key", type="password")
