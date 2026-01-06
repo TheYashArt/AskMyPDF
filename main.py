11c3271539942233c34337932b4373fda18e41e2
TheYashArt <shelkey859@gmail.com>
2026-01-06 17:31:36 +0530

    file name update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # 1. Define the pages
 pdf_page = st.Page("pages/PDF_Exploration.py", title="PDF Chat", icon="📄")
-yt_page = st.Page("pages/Youtube_Summurizer.py", title="YouTube Summary", icon="🎥")
+yt_page = st.Page("pages/Youtube_Summarizer.py", title="YouTube Summary", icon="🎥")
 
 # 2. Setup Navigation
 # Only the pages in this list will show up in the sidebar
